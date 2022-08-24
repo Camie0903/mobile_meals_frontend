365846c819148cdf9e5a6e7d0dc1c90cddd892d5
Camie0903 <camerontamboer03@gmail.com>
2022-08-24 11:07:19 +0200

    second commit

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,17 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    users: null,
+    token: null,
+    products: null,
+    product: null,
   },
   getters: {
   },
   mutations: {
+    setToken: (state, token) => {
+      state.token = token;
+    }
   },
   actions: {
   },
